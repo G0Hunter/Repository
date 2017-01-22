420d82928b6d07aeb8de3079d6f97db236c9a43b
G0Hunter <kondrashkin403@mail.ru>
2017-01-23 01:37:30 +0200

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,52 +1,73 @@
+;var slider = (function() {
     var $caruselClass = $('.carousel');
-    //click on right button
-    $(document).on('click', '.carousel-button-right', function() {
-        right_carusel();
-        return false;
-    });
-    //click  on left button
-    $(document).on('click', '.carousel-button-left', function() {
-        left_carusel();
-        return false;
-    });
-    function left_carusel() {
-        var block_width = $caruselClass.find('.carousel-block').outerWidth();
-        $caruselClass.find('.carousel-items .carousel-block').eq(-1).clone().prependTo($caruselClass.find('.carousel-items'));
-        $caruselClass.find('.carousel-items').css({
-            'left': '-' + block_width + 'px'
-        });
-        $caruselClass.find('.carousel-items .carousel-block').eq(-1).remove();
-        $caruselClass.find('.carousel-items').animate({
-            left: "0px"
-        }, 0);
-    }
-    function right_carusel(carusel) {
-        var block_width = $caruselClass.find('.carousel-block').outerWidth();
-        $caruselClass.find('.carousel-items').animate({
-            left: '-' + block_width + 'px'
-        }, 0, function() {
-            $caruselClass.find('.carousel-items .carousel-block').eq(0).clone().appendTo($caruselClass.find('.carousel-items'));
-            $caruselClass.find('.carousel-items .carousel-block').eq(0).remove();
+    var fnCarusel = {
+        leftCarusel: function() {
+            var block_width = $caruselClass.find('.carousel-block').outerWidth();
+            $caruselClass.find('.carousel-items .carousel-block').eq(-1).clone().prependTo($caruselClass.find('.carousel-items'));
             $caruselClass.find('.carousel-items').css({
-                'left': '0px'
+                'left': '-' + block_width + 'px'
             });
-        });
-    }
-    // auto 
-    $(function() {
-        auto_right('.carousel:first');
-    })
-    function auto_right(carusel) {
-        setInterval(function() {
-            if (!(($caruselClass).is('.hover'))) {
-                right_carusel($caruselClass);
+            $caruselClass.find('.carousel-items .carousel-block').eq(-1).remove();
+            $caruselClass.find('.carousel-items').animate({
+                left: "0px"
+            }, 0);
+        },
+        rightCarusel: function() {
+            var block_width = $caruselClass.find('.carousel-block').outerWidth();
+            $caruselClass.find('.carousel-items').animate({
+                left: '-' + block_width + 'px'
+            }, 0, function() {
+                $caruselClass.find('.carousel-items .carousel-block').eq(0).clone().appendTo($caruselClass.find('.carousel-items'));
+                $caruselClass.find('.carousel-items .carousel-block').eq(0).remove();
+                $caruselClass.find('.carousel-items').css({
+                    'left': '0px'
+                });
+            });
+        }
+    };
+    var fnCaruselInit = {
+        // auto 
+        autoRight: function() {
+            setInterval(function() {
+                if (!(($caruselClass).is('.hover'))) {
+                    fnCarusel.rightCarusel($caruselClass);
+                }
+            }, 4000)
+        },
+        //click on right button
+        flipRight: function() {
+            $(document).on('click', '.carousel-button-right', function() {
+                fnCarusel.rightCarusel();
+                return false;
+            });
+        },
+        //click  on left button
+        flipLeft: function() {
+            $(document).on('click', '.carousel-button-left', function() {
+                fnCarusel.leftCarusel();
+                return false;
+            });
+        },
+        // Stop when mouse over slide
+        addHover: function() {
+            $(document).on('mouseenter', '.carousel', function() {
+                $(this).addClass('hover');
+            })
+        },
+        removeHover: function() {
+            $(document).on('mouseleave', '.carousel', function() {
+                $(this).removeClass('hover');
+            });
+        }
+    };
+    return {
+        init: function() {
+            for (var prop in fnCaruselInit) {
+                fnCaruselInit[prop]();
             }
-        }, 4000)
+        }
     }
-    // Stop when mouse over slide
-    $(document).on('mouseenter', '.carousel', function() {
-        $(this).addClass('hover');
-    })
-    $(document).on('mouseleave', '.carousel', function() {
-        $(this).removeClass('hover');
-    });
+}());
+$(function() {
+    slider.init();
+});
